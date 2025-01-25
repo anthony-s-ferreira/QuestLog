668abcb53250ba27ffa5da5eb59db1589c2b4807
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-01-25 15:27:07 -0300

    refactor: transform server from JS into TS
    Co-Authored-By: Bruno Grangeiro Bonifácio <109104329+Bruno-Grangeiro@users.noreply.github.com>

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app: express.Application = express();
+const port = process.env.PORT || 3000;
+
+//API routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

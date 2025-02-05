a3703dfdfa3706fd021f7f1439f032e7364cab75
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-05 20:36:54 -0300

    feat: add routes and db to app
    Co-Authored-By: Bruno Grangeiro Bonifácio <109104329+Bruno-Grangeiro@users.noreply.github.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,31 @@
 import express, { Request, Response } from 'express';
 import { setupSwagger } from '../config/swagger';
+import sequelize from '../config/database';
+import { error } from 'console';
+import { userRoutes } from './routes/userRoutes';
+
+
 const app: express.Application = express();
 const port = process.env.PORT || 3000;
+
+//Configurations
+app.use(express.json());
+
 
 //API routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
 });
+app.use(userRoutes);
 
 // Setup Swagger
 setupSwagger(app);
-
+sequelize.sync({force: true}).then(() => {
+  console.log('Database connected');
+}).catch((error) => {
+  console.log('An error during bd connection occurred: ', error);
+  process.exit();
+});
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   console.log(`Swagger available at http://localhost:${port}/api-docs`);

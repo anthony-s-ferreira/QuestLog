c8d6c26d3466a1715aaa84781dd2850925793ca5
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-05 20:35:57 -0300

    feat: create user controller
    Co-Authored-By: Bruno Grangeiro Bonifácio <109104329+Bruno-Grangeiro@users.noreply.github.com>

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { UserService } from '../services/UserService';
+
+const userService = new UserService();
+
+export const createUser = async (req: Request, res: Response) => {
+    const { name, email, password, type } = req.body;
+
+    try {
+        const user = await userService.createUser(name, email, password, type);
+        res.status(201).json(user);
+    } catch (error: Error | any) {
+        res.status(400).json({ message: "Erro ao criar usuário", error: error.message });
+    }
+};
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await userService.getAllUsers();
+        res.status(200).json(users);
+    } catch (error: Error | any) {
+        res.status(500).json({ message: "Erro ao obter usuários", error: error.message });
+    }
+}

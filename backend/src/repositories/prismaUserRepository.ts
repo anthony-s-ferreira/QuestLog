cfe545ace5df131ee16a9898edd3300e8b88a7e7
Bruno-Grangeiro <bruno.bonifacio@ccc.ufcg.edu.br>
2025-02-11 20:06:42 -0300

    feat: add delete, patch and update for users on prisma

--- backend/src/repositories/prismaUserRepository.ts
+++ backend/src/repositories/prismaUserRepository.ts
@@ -1,13 +1,31 @@
 import { db } from "../../config/database";
 
-export const createUser = ( input: any ) => {
+export const createUser = (input: any) => {
     return db.user.create({ data: input });
-}
+};
 
 export const getAllUsers = () => {
     return db.user.findMany();
-}
+};
 
 export const getUserById = (id: number) => {
-    return db.user.findUnique({where: {id}});
-}
+    return db.user.findUnique({ where: { id } });
+};
+
+export const updateUser = (id: number, data: any) => {
+    return db.user.update({
+        where: { id },
+        data
+    });
+};
+
+export const updateUserPassword = (id: number, newPassword: string) => {
+    return db.user.update({
+        where: { id },
+        data: { password: newPassword }
+    });
+};
+
+export const deleteUserById = (id: number) => {
+    return db.user.delete({ where: { id } });
+};

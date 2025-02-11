b8041ab941840abfe742da5519ed0dd497f3883f
Bruno-Grangeiro <bruno.bonifacio@ccc.ufcg.edu.br>
2025-02-11 20:07:45 -0300

    feat: add delete, patch and update for users on service

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,11 +1,11 @@
 import * as repository from "../repositories/prismaUserRepository";
-import { User, UserType } from "../domain/entities/User"; // Adjust the import path as necessary
+import { User, UserType } from "../domain/entities/User";
 
 export class UserService {
 
     async createUser(name: string, email: string, password: string, type: string) {
-        const user = {name, email, password, type} as User;
-        return await repository.createUser(user);
+        const user = { name, email, password, type: type as UserType } as User;
+        return repository.createUser(user);
     }
 
     async getAllUsers() {
@@ -14,5 +14,33 @@ export class UserService {
 
     async getUserById(id: number) {
         return await repository.getUserById(id);
+    }
+
+    async updateUser(id: number, name: string, email: string, type: string) {
+        const user = await this.getUserById(id);
+        if (!user) {
+            throw new Error("User not found.");
+        }
+
+        user.name = name;
+        user.email = email;
+        user.type = type as UserType;
+
+        return repository.updateUser(id, user);
+    }
+
+    async updateUserPassword(id: number, newPassword: string) {
+        const user = await this.getUserById(id);
+        if (!user) {
+            throw new Error("User not found.");
+        }
+
+        user.password = newPassword;
+
+        return repository.updateUserPassword(id, newPassword);
+    }
+
+    async deleteUser(id: number) {
+        return repository.deleteUserById(id);
     }
 }

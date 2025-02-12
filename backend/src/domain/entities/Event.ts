a0edc9a31e6c75b42361b1ab2ad57bfd6ab13470
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-11 23:22:52 -0300

    feat: add event model

---
+++ backend/src/domain/entities/Event.ts
@@ -0,0 +1,10 @@
+import { Character } from "./Character";
+import { EventType } from "./EventType";
+
+export interface Event{
+    id?: number;
+    description: string;
+    createdAt?: Date;
+    character: Character;
+    type: EventType;
+}

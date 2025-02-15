8f8e6337e15179b5e662ca05d7a07a9ee3ab06aa
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-15 16:41:29 -0300

    feat: add validatos to event type service

--- backend/src/services/EventTypeService.ts
+++ backend/src/services/EventTypeService.ts
@@ -1,25 +1,47 @@
+import { EventTypeDTO } from '../domain/DTO/EventTypeDTO';
+import { EventType } from '../domain/entities/EventType';
+import { EventTypeFormDTO } from '../domain/formDTO/EventTypeFormDTO';
 import * as repository from '../repositories/prismaEventTypeRepository'
+import { validateId } from '../validators/CommonValidator';
+import { validateEventTypeDescription, validateEventTypeExists, validateEventTypeId, validateEventTypeName } from '../validators/EventTypeValidator';
 
 export class EventTypeService {
-    async createEventType(name: string, description: string) {
-        const eventType = {name, description};
-        return await repository.createEventType(eventType);
+    async createEventType(eventTypeForm: EventTypeFormDTO) {
+        validateEventTypeName(eventTypeForm.name);
+        validateEventTypeDescription(eventTypeForm.description);
+        const eventType = await repository.createEventType(eventTypeForm);
+        return this.convertEventType(eventType);
     }
 
-    async updateEventType(id: number, name: string, description: string) {
-        const eventType = {id, name, description};
-        return await repository.updateEventType(id, eventType);
+    async updateEventType(id: number, eventTypeForm: EventTypeFormDTO) {
+        validateId(id, 'Event Type');
+        await validateEventTypeExists(id);
+        validateEventTypeName(eventTypeForm.name);
+        validateEventTypeDescription(eventTypeForm.description);
+        const eventType = {id, name: eventTypeForm.name, description: eventTypeForm.description};
+        const updatedEventType =  await repository.updateEventType(id, eventType);
+        return this.convertEventType(updatedEventType);
     }
 
     async getEventTypes() {
-        return await repository.getEventTypes();
+        const eventsType = await repository.getEventTypes();
+        return eventsType.map(eventType => this.convertEventType(eventType));
     }
 
     async getEventTypeById(id: number) {
-        return await repository.getEventType(id);
+        validateId(id, 'Event Type');
+        await validateEventTypeExists(id);
+        const eventType = await repository.getEventType(id);
+        return this.convertEventType(eventType);
     }
 
     async deleteEventType(id: number) {
+        validateId(id, 'Event Type');
+        await validateEventTypeExists(id);
         return await repository.deleteEventType(id);
+    }
+
+    convertEventType(eventType: EventType) : EventTypeDTO {
+        return { id: eventType.id, name: eventType.name, description: eventType.description };
     }
 }

69f7a345751642996bb1e93a2444a1e2dca2e916
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-15 17:29:56 -0300

    feat: fix rpg repository

--- backend/src/repositories/prismaRpgRepository.ts
+++ backend/src/repositories/prismaRpgRepository.ts
@@ -1,65 +1,62 @@
+import { RPG } from "@prisma/client";
 import { db } from "../../config/database";
 
 export const createRpg = ( input: any ) => {
-    return db.rPG.create({ data: input, include: {
-        master: {
-            select: {
-                id: true,
-                name: true,
+    return db.rPG.create(
+        { 
+            data: input, 
+            include: {
+                master: true 
             }
         }
-    } });
+    );
 }
 
 export const getAllRpgs = () => {
-    return db.rPG.findMany( {
-        include: {
-            master: {
-                select: {
-                    id: true,
-                    name: true,
-                }
+    return db.rPG.findMany(
+        {
+            include: {
+                master: true
             }
         }
-    });
+    )
 };
 
 
 export const getRpgById = ( id: number ) => {
-    return db.rPG.findUnique({ where: { id }, include: {
-        master: {
-            select: {
-                id: true,
-                name: true,
+    return db.rPG.findUnique(
+        { 
+            where: { id }, 
+            include: {
+                master: true
             }
         }
-    } });
+    );
 }
 
 export const updateRpg = ( id: number, input: any ) => {
-    return db.rPG.update({ where: { id }, data: input, include: {
-        master: {
-            select: {
-                id: true,
-                name: true,
+    return db.rPG.update(
+        {
+            where: { id }, 
+            data: input, 
+            include: {
+                master: true
             }
         }
-    } });
+    );
 }
 
-export const updateRPGStatus = async (id: number, active: boolean) => {
-    const rpg = await getRpgById(id);
-    if (rpg) {
-        rpg.active = active;
-        return db.rPG.update({where: {id}, data: rpg, include: {
-            master: {
-                select: {
-                    id: true,
-                    name: true,
-                }
+export const updateRPGStatus = async (id: number, rpg: RPG) => {
+
+    return db.rPG.update(
+        {
+            where: {id}, 
+            data: rpg, 
+            include: {
+                master: true
             }
-        }});
-    }
+        }
+    );
 }
 
 export const deleteRPGById = (id: number) => {

3c2591757e289d7de109411ea744ec53a8fdb368
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-15 16:41:55 -0300

    feat: update event type validator

--- backend/src/validators/EventTypeValidator.ts
+++ backend/src/validators/EventTypeValidator.ts
@@ -1,55 +1,64 @@
 import { Response } from 'express';
 import { EventType } from '../domain/entities/EventType';
 import { EventTypeService } from '../services/EventTypeService';
+import { validateId } from './CommonValidator';
+import { EventTypeFormDTO } from '../domain/formDTO/EventTypeFormDTO';
 
 const eventTypeService = new EventTypeService()
 
-export const validateRequestBody = (body: EventType, res: Response) => {
+export const validateRequestBody = (body: EventTypeFormDTO, res: Response) => {
     if (Object.keys(body).length === 0) {
         res.status(400).json({ message: 'Event Type is required.' });
         throw new Error('Event Type is required.');
-      }
+    }
+    try {
+        validateEventTypeName(body.name);
+        validateEventTypeDescription(body.description);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 }
 
-export const validateEventTypeName = (name: string, res: Response) => {
+export const validateEventTypeName = (name: string) => {
     if (!name || name.trim() === "") {
-        res.status(400).json({message: 'Event Type name is required.'});
         throw new Error('Event Type name is required.');
     }
     if (name.length < 3) {
-        res.status(400).json({message: 'Event Type name must have at least 3 characters.'});
         throw new Error('Event Type name must have at least 3 characters.');
     }
     if (name.length > 20) {
-        res.status(400).json({message: 'Event Type name must have at most 20 characters.'});
         throw new Error('Event Type name must have at most 20 characters.');
     }
 }
 
-export const validateEventTypeDescription = (description: string, res: Response) => {
+export const validateEventTypeDescription = (description: string) => {
     if (!description || description.trim() === "") {
-        res.status(400).json({message: 'Event Type description is required.'});
         throw new Error('Event Type description is required.');
     }
     if (description.length < 20) {
-        res.status(400).json({message: 'Event Type description must have at least 3 characters.'});
         throw new Error('Event Type description must have at least 3 characters.');
     }
     if (description.length > 200) {
-        res.status(400).json({message: 'Event Type description must have at most 200 characters.'});
         throw new Error('Event Type name must have at most 200 characters.');
     }
 }
 
-
-export const validateEventTypeId = async (id: number, res: Response) => {
-    if (!id) {
-        res.status(400).json( { message: 'Event Type id is required.' } );
-        throw new Error('Event Type id is required.');
-    }
+export const validateEventTypeExists = async (id: number) => {
     const eventType = await eventTypeService.getEventTypeById(id);
     if (!eventType) {
-        res.status(404).json( {message: `Event Type with id: ${id} does not exist.`} );
         throw new Error(`Event Type with id: ${id} does not exist.`);
+    }
+}
+
+export const validateEventTypeId = async (id: number, res: Response) => {
+    try {
+        validateId(id, 'Event Type');
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    try {
+        validateEventTypeExists(id);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 }

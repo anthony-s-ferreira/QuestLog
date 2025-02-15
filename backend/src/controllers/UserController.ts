c389835a5409028fce6bfb9df7eb8de1ebd7b13e
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-02-15 16:23:04 -0300

    feat: add validators to user controller

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from 'express';
 import { UserService } from '../services/UserService';
+import { validateRequestBody, validateUserId, validateUserPasswordUpdate } from '../validators/UserValidator';
+import { UserFormDTO } from '../domain/formDTO/UserFormDTO';
 
 const userService = new UserService();
 
 export const createUser = async (req: Request, res: Response) => {
     const { name, email, password, type } = req.body;
-
+    const userForm: UserFormDTO= {name, email, password, type};
     try {
+        validateRequestBody(userForm, res);
         const user = await userService.createUser(name, email, password, type);
         res.status(201).json(user);
     } catch (error: Error | any) {
-        res.status(400).json({ message: "Error creating user", error: error.message });
+        console.log({ message: "Error creating user", error: error.message });
     }
 };
 
@@ -24,43 +27,42 @@ export const getAllUsers = async (req: Request, res: Response) => {
 };
 
 export const getUserById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-
+    let { id } = req.params;
     try {
+        await validateUserId(Number(id), res);
         const user = await userService.getUserById(Number(id));
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
         res.status(200).json(user);
     } catch (error: Error | any) {
-        res.status(500).json({ message: "Error retrieving user", error: error.message });
+        console.log({ message: "Error retrieving user", error: error.message });
     }
 };
 
 export const updateUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const { name, email, type } = req.body;
+    const { name, email, type, password } = req.body;
+    const userForm: UserFormDTO= {name, email, password, type};
 
     try {
+        await validateUserId(Number(id), res);
+        validateRequestBody(userForm, res);
         const updatedUser = await userService.updateUser(Number(id), name, email, type);
         res.status(200).json(updatedUser);
     } catch (error: Error | any) {
-        res.status(400).json({ message: "Error updating user", error: error.message });
+        console.log({ message: "Error updating user", error: error.message });
     }
 };
 
 export const updateUserPassword = async (req: Request, res: Response) => {
     const { id } = req.params;
-    const { password } = req.body;
+    const { password, newPassword } = req.body;
 
     try {
-        if (!password) {
-            return res.status(400).json({ message: "Password is required" });
-        }
-        await userService.updateUserPassword(Number(id), password);
+        await validateUserId(Number(id), res);
+        await validateUserPasswordUpdate(Number(id), password, newPassword, res);
+        await userService.updateUserPassword(Number(id), newPassword);
         res.status(200).json({ message: "Password updated successfully" });
     } catch (error: Error | any) {
-        res.status(400).json({ message: "Error updating password", error: error.message });
+        console.log({ message: "Error updating password", error: error.message });
     }
 };
 
@@ -68,14 +70,10 @@ export const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
 
     try {
-        const user = await userService.getUserById(Number(id));
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
+        await validateUserId(Number(id), res);
         await userService.deleteUser(Number(id));
         res.status(200).json({ message: "User deleted successfully" });
     } catch (error: Error | any) {
-        res.status(500).json({ message: "Error deleting user", error: error.message });
+        console.log({ message: "Error deleting user", error: error.message });
     }
 };

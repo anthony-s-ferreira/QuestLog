b980bdbfbc7abaafa75a700dd7f255c82817bf22
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-03-03 14:10:29 -0300

    fix: separate return attribute

--- backend/src/config/jwt.ts
+++ backend/src/config/jwt.ts
@@ -3,7 +3,8 @@ import jwt from "jsonwebtoken";
 const secret = process.env.SECRET_KEY;
 
 export const generateToken = (id: number): string => {
-    return jwt.sign({ id }, secret, { expiresIn: "1h"});
+    const token = jwt.sign({ id }, secret, { expiresIn: "1h"})
+    return token;
 };
 
 export const verifyToken = (token: string) => {

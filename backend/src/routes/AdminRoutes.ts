89833507fddc52508c3b6a90d9beb1a2c5cfcec9
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-03-14 18:50:01 -0300

    feat: update admin routes with pagination

--- backend/src/routes/AdminRoutes.ts
+++ backend/src/routes/AdminRoutes.ts
@@ -15,15 +15,27 @@ const router = Router();
  *   description: Endpoints for managing admin functions
  */
 
-
 /**
  * @swagger
  * /admin/rpgs:
  *   get:
  *     summary: Get all RPGs
- *     description: Retrieves a list of all RPGs.
+ *     description: Retrieves a list of all RPGs with pagination.
  *     tags:
  *       - Admin
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The page number
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The number of RPGs per page
  *     responses:
  *       200:
  *         description: List of RPGs retrieved successfully.
@@ -35,9 +47,22 @@ router.get('/admin/rpgs', authMiddleware, AdminPermissionMiddleware, getAllRPGs)
  * /admin/characters:
  *   get:
  *     summary: Get all Characters
- *     description: Retrieves a list of all Characters in the system.
+ *     description: Retrieves a list of all Characters in the system with pagination.
  *     tags:
  *       - Admin
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The page number
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The number of characters per page
  *     responses:
  *       200:
  *         description: List of Characters retrieved successfully.
@@ -51,9 +76,22 @@ router.get('/admin/characters', authMiddleware, AdminPermissionMiddleware, getAl
  * /admin/events:
  *   get:
  *     summary: Retrieves all events
- *     description: Returns a list of all events in the system.
+ *     description: Returns a list of all events in the system with pagination.
  *     tags:
  *       - Admin
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The page number
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The number of events per page
  *     responses:
  *       200:
  *         description: List of events retrieved successfully.
@@ -67,29 +105,26 @@ router.get("/admin/events", authMiddleware, AdminPermissionMiddleware, getEvents
  * /admin/users:
  *   get:
  *     summary: Get a list of users
- *     description: Returns all registered users.
+ *     description: Returns all registered users with pagination.
  *     tags:
  *       - Admin
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The page number
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: The number of users per page
  *     responses:
  *       200:
  *         description: Successfully retrieved user list.
  */
 router.get("/admin/users", authMiddleware, AdminPermissionMiddleware, getAllUsers);
-
-/**
- * @swagger
- * /admin/events:
- *   get:
- *     summary: Retrieves all events
- *     description: Returns a list of all events in the system.
- *     tags:
- *       - Admin
- *     responses:
- *       200:
- *         description: List of events retrieved successfully.
- *       500:
- *         description: Internal server error.
- */
-router.get("/admin/events", authMiddleware, AdminPermissionMiddleware, getEvents);
 
 export { router as adminRoutes };

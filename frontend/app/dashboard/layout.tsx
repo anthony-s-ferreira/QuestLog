976b75a45161afe4d2354b2bf64bf7cc27804487
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-03-28 21:28:02 -0300

    feat: add loading to user fetch

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,13 +1,17 @@
+"use client"
 import type React from "react"
 import { DashboardHeader } from "@/components/dashboard/dashboard-header"
 import { DashboardSidebar } from "@/components/dashboard/dashboard-sidebar"
-import { Toaster } from "@/components/ui/toaster"
+import { useAuth } from "@/context/AuthContext"
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const { user, loading: loadingAuth } = useAuth(); 
+  if (loadingAuth) return <p>Carregando...</p>; 
+
   return (
     <div className="flex min-h-screen flex-col">
       <DashboardHeader />

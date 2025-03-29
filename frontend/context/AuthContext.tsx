184b0434b09b927702daa59be3c5b13cfc09b817
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-03-28 21:27:11 -0300

    feat: update authcontext to use cookies

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -1,14 +1,25 @@
 "use client";
+
 import { createContext, useContext, useState, useEffect, ReactNode } from "react";
 import { useRouter } from "next/navigation";
 import { authService } from "@/services/authService";
-import { api } from "@/services/api"; // Axios configurado
+import {api} from "@/services/api"; // Axios configurado
+import Cookies from "js-cookie"
+import { jwtDecode } from 'jwt-decode'
+
 
 interface User {
   id: number;
   name: string;
   email: string;
   type: string;
+}
+
+interface JwtPayload {
+  id: string
+  name: string
+  email: string
+  type: string
 }
 
 interface AuthContextData {
@@ -24,36 +35,47 @@ const AuthContext = createContext<AuthContextData | null>(null);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
   const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = Cookies.get("authToken");
     if (token) {
-      api.defaults.headers.Authorization = `Bearer ${token}`;
-      fetchUser(); // Busca os dados do usuário autenticado
+      try {
+        fetchUser();
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+      } catch (error) {
+        console.error("Erro ao decodificar o token:", error);
+        signOut();
+        setLoading(false);
+      }
     } else {
       setLoading(false);
     }
   }, []);
+  
+
 
   const fetchUser = async () => {
     try {
       const response = await api.get("/user/me"); // Endpoint para retornar o usuário autenticado
       setUser(response.data);
+      setLoading(false);
     } catch (error) {
       console.error("Erro ao buscar usuário autenticado:", error);
       signOut();
-    } finally {
-      setLoading(false);
     }
   };
 
   const signIn = async (email: string, password: string) => {
     try {
+
       const { token, user } = await authService.login(email, password);
-      localStorage.setItem("token", token);
-      api.defaults.headers.Authorization = `Bearer ${token}`;
+      Cookies.set("authToken", token, {
+        expires: 7,
+        secure: true,
+        sameSite: "Strict"
+      })
       await fetchUser();
       router.push("/dashboard");
     } catch (error) {
@@ -63,16 +85,17 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
   const signOut = () => {
     setUser(null);
-    localStorage.removeItem("token");
+    Cookies.remove("authToken")
     api.defaults.headers.Authorization = "";
+    router.push("/login");
   };
-
+  
   const isAdmin = () => {
     return user?.type === "admin";
-  };
+  }
 
   return (
-    <AuthContext.Provider value={{ user, signIn, signOut, isAdmin, loading, fetchUser }}>
+    <AuthContext.Provider value={{ user, signIn, signOut, isAdmin, fetchUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
@@ -80,7 +103,6 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-
   if (!context) throw new Error("useAuth deve ser usado dentro de um AuthProvider");
   return context;
 };

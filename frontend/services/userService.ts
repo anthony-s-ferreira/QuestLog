5413466b1ba71c793d7a981d2a7d1d5c10b5a3b4
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2025-03-31 18:11:15 -0300

    feat: add signup method frontend

--- frontend/services/userService.ts
+++ frontend/services/userService.ts
@@ -15,6 +15,21 @@ export async function updateUser(id: number, userData: { name: string; email: st
     
 }
 
+export async function signupUser(userData: {name: string; email: string; password: string;}) {
+    try {
+        const signUpData = {
+            name: userData.name,
+            email: userData.email,
+            password: userData.password,
+            type: 'user'
+        }
+        const response = await api.post('/register', signUpData);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
 export async function updateUserPassword(id: number, passwordData: { password: string; newPassword: string }) {
     const response = await api.patch(`/users/${id}/password`, passwordData);
     return response.data;
